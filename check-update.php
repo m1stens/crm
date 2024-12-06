1dbbc93363719f17f64ac8e1ff991045c7f2e48a
m1stens <128590432+m1stens@users.noreply.github.com>
2024-12-06 11:46:32 +0200

    Update check-update.php

--- check-update.php
+++ check-update.php
@@ -3,10 +3,9 @@
 
 	$id = $_POST['id'];
 	$name = $_POST['username'];
-	$age = $_POST['age'];
 	$phone = $_POST['phone'];
 	$email = $_POST['email'];
 
-    $sql ="UPDATE `TClient` SET `fFIO` = '$name', `fAge` = '$age', `fPhone_number` = '$phone', `fEmail` = '$email' WHERE `TClient`.`fid_client` = '$id'";
+    $sql ="UPDATE `TClient` SET `fFIO` = '$name', `fPhone_number` = '$phone', `fEmail` = '$email' WHERE `TClient`.`fid_client` = '$id'";
 	mysqli_query($conn, $sql);
 	header('Location: client.php');
